5d314f9c4ca2ca429761e0a1eb8534fcd3887d1f
Giancarlo Anemone <ganemone@uber.com>
2018-10-31 13:13:56 -0400

    Include src/ in published files, fix flow errors

--- src/utils/create-restore-point.test.js
+++ src/utils/create-restore-point.test.js
@@ -28,7 +28,10 @@ import {createRestorePoint} from './create-restore-point.js';
 
 test('createRestorePoint', async () => {
   const file = '__restore_point__.json';
-  await createRestorePoint(file, new StepperError('error', 'step', 0));
+  await createRestorePoint(
+    file,
+    new StepperError(new Error('error'), 'step', 0),
+  );
   await expect(fse.pathExists(file)).resolves.toEqual(true);
   await fse.remove(file);
 });
